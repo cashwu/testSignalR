b012747203fd71a2aba220ccc2eee5812998f935
Cash Wu <cash@cashwu.com>
2016-09-21 10:50:19 +0800

    signalR  tracking  test  project

---
+++ testSignalR/Scripts/tracking.js
@@ -0,0 +1,71 @@
+﻿
+$(document).ready(function () {
+    var connection = $.connection("/tracker");
+
+    connection.start(function() {
+        startTracking();
+    });
+
+    connection.received(function(data) {
+
+
+        data = JSON.parse(data);
+
+        var domElementId = "id" + data.id;
+
+        var elem = createElementIfNotExists(domElementId);
+        $(elem)
+            .css({
+                left: data.x,
+                top: data.y
+            })
+            .text(data.id);
+    });
+
+    function startTracking() {
+        $("body")
+            .mousemove(function (e) {
+                console.log(e);
+                var data = { x: e.pageX, y: e.pageY, id: connection.id };
+                
+                connection.send(data);
+            });
+    }
+
+    function createElementIfNotExists(id) {
+        var element = $("#" + id);
+
+        if (element.length === 0) {
+
+            element = $("<span class='client' " + "id='" + id + "'></span>");
+
+            var color = getRandomColor();
+
+            element.css({
+                backgroundColor : getRgb(color),
+                color : getInverseRgb(color)
+            });
+
+            $("body").append(element).show();
+        }
+
+        return element;
+    }
+
+    function getRgb(rgb) {
+        return "rgb(" + rgb.r + "," + rgb.g + "," + rgb.b + ")";
+    }
+
+    function getInverseRgb(rgb) {
+        return "rgb(" + (255 - rgb.r) + "," + (255 - rgb.g) + "," + (255 - rgb.b) + ")";
+    }
+
+    function getRandomColor() {
+        return {
+            r: Math.round(Math.random() * 256),
+            g: Math.round(Math.random() * 256),
+            b: Math.round(Math.random() * 256)
+        };
+    }
+
+});
---
+++ testSignalR/TrackerConnection.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.SignalR;
+using System.Threading.Tasks;
+
+namespace testSignalR
+{
+    public class TrackerConnection : PersistentConnection
+    {
+        //protected override Task OnConnected(IRequest request, string connectionId)
+        //{
+        //    return Connection.Send(connectionId, "Welcome!");
+        //}
+
+        protected override Task OnReceived(IRequest request, string connectionId, string data)
+        {
+            return Connection.Broadcast(data);
+        }
+    }
+}
